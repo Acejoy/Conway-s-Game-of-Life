9ebd01f6eacb3314886b7134d9a485b0e36ed3e5
Acejoy <srinjoym98@gmail.com>
2021-05-24 18:07:41 +0530

    implemented all the functions , result-functional

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
-const cellSize = 20;
+const cellSize = 10;
+const interval = 100;
 var liveCells = new Array();
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
@@ -6,12 +7,12 @@ var context = canvas.getContext("2d");
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
-const numXCells = Math.floor(canvasWidth/cellSize);
-const numYCells = Math.floor(canvasHeight/cellSize);
+const numXCells = Math.floor(canvasWidth / cellSize);
+const numYCells = Math.floor(canvasHeight / cellSize);
 
 function getRecCordinates(x_coord, y_coord) {
-    rec_x_coord = Math.floor(x_coord/cellSize);
-    rec_y_coord = Math.floor(y_coord/cellSize);
+    rec_x_coord = Math.floor(x_coord / cellSize);
+    rec_y_coord = Math.floor(y_coord / cellSize);
 
     return [rec_x_coord, rec_y_coord];
 };
@@ -21,22 +22,52 @@ function getCursorPosition(canvas, event, arr, canvasContext) {
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
     arr.push(getRecCordinates(x, y));
-    drawPixel(canvasContext, arr);
+    drawPixels(canvasContext, arr, "#a2a4a6");
     console.log("x: " + x + " y: " + y);
 };
 
-function drawPixels(canvasContext, arr) {
-    for(ind=0; ind<arr.length; ind++) {
-        x_coord = arr[ind][0]*cellSize;
-        y_coord = arr[ind][1]*cellSize;
+function drawPixels(canvasContext, arr, style) {
+
+    canvasContext.fillStyle = style;
+
+    for (ind = 0; ind < arr.length; ind++) {
+        x_coord = arr[ind][0] * cellSize;
+        y_coord = arr[ind][1] * cellSize;
         canvasContext.fillRect(x_coord, y_coord, cellSize, cellSize);
     }
+
 };
 
-function isLive(liveArray, coords){
-    
-    for(idx=0; idx<liveArray.length; idx++){
-        if(JSON.stringify(coords)==JSON.stringify(liveArray[idx])){
+function drawRectangle(canvasContext, arr) {
+    canvasContext.strokeStyle = "#97999c";
+    canvasContext.fillStyle = "#FFFFFF";
+
+    for (ind = 0; ind < arr.length; ind++) {
+        x_coord = arr[ind][0] * cellSize;
+        y_coord = arr[ind][1] * cellSize;
+
+        canvasContext.fillRect(x_coord, y_coord, cellSize, cellSize);
+
+        canvasContext.moveTo(x, y);
+        canvasContext.lineTo(x, y + cellSize);
+
+        canvasContext.moveTo(x, y + cellSize);
+        canvasContext.lineTo(x + cellSize, y + cellSize);
+
+        canvasContext.moveTo(x + cellSize, y + cellSize);
+        canvasContext.lineTo(x + cellSize, y);
+
+        canvasContext.moveTo(x + cellSize, y);
+        canvasContext.lineTo(x, y);        
+    }
+
+    context.stroke();
+};
+
+function isLive(liveArray, coords) {
+
+    for (idx = 0; idx < liveArray.length; idx++) {
+        if (coords[0] == liveArray[idx][0] && coords[1] == liveArray[idx][1]) {
             return true;
         }
     }
@@ -45,76 +76,97 @@ function isLive(liveArray, coords){
 };
 
 function getNumLiveNeighbours(liveArray, curCoord) {
-    let numNeighbors = 0;
+    var numNeighbors = 0;
     //cur coordinate -> (x,y)
     var x = curCoord[0];
     var y = curCoord[1];
 
     //check for (x+1, y)
-    if(x!=numXCells-1 && isLive(liveArray, [curCoord[0]+1, curCoord[1]])){
+    if (x != numXCells - 1 && isLive(liveArray, [curCoord[0] + 1, curCoord[1]])) {
         numNeighbors++;
     }
 
     //check for (x+1, y+1)
-    if((x!=numXCells-1 || y!=numYCells-1) && isLive(liveArray, [curCoord[0]+1, curCoord[1]+1])){
+    if ((x != numXCells - 1 || y != numYCells - 1) && isLive(liveArray, [curCoord[0] + 1, curCoord[1] + 1])) {
         numNeighbors++;
     }
 
     //check for (x, y+1)
-    if(y!=numYCells-1 && isLive(liveArray, [curCoord[0], curCoord[1]+1])){
+    if (y != numYCells - 1 && isLive(liveArray, [curCoord[0], curCoord[1] + 1])) {
         numNeighbors++;
     }
 
     //check for (x-1, y+1)
-    if((x==0 || y!=numYCells-1) && isLive(liveArray, [curCoord[0]-1, curCoord[1]+1])){
+    if ((x == 0 || y != numYCells - 1) && isLive(liveArray, [curCoord[0] - 1, curCoord[1] + 1])) {
         numNeighbors++;
     }
 
     //check for (x-1, y)
-    if((x!=0) && isLive(liveArray, [curCoord[0]-1, curCoord[1]])){
+    if ((x != 0) && isLive(liveArray, [curCoord[0] - 1, curCoord[1]])) {
         numNeighbors++;
     }
 
     //check for (x-1, y-1)
-    if((x!=0 || y!=0) && isLive(liveArray, [curCoord[0]-1, curCoord[1]-1])){
+    if ((x != 0 || y != 0) && isLive(liveArray, [curCoord[0] - 1, curCoord[1] - 1])) {
         numNeighbors++;
     }
 
     //check for (x, y-1)
-    if(y!=0 && isLive(liveArray, [curCoord[0], curCoord[1]-1])){
+    if (y != 0 && isLive(liveArray, [curCoord[0], curCoord[1] - 1])) {
         numNeighbors++;
     }
 
     //check for (x+1, y-1)
-    if((x!=numXCells-1 && y!=0) && isLive(liveArray, [curCoord[0]+1, curCoord[1]-1])){
+    if ((x != numXCells - 1 && y != 0) && isLive(liveArray, [curCoord[0] + 1, curCoord[1] - 1])) {
         numNeighbors++;
     }
 
     return numNeighbors;
 };
 
-function updateLiveArray(canvas,liveArray){
+function updateLiveArray(liveArray) {
 
     var newArr = new Array();
     var neighbors = 0;
-    for(ind_x=0; ind_x<numXCells; ind_x++){
-        for(ind_y=0; ind_y<numYCells; ind_y++){
+    for (ind_x = 0; ind_x < numXCells; ind_x++) {
+        for (ind_y = 0; ind_y < numYCells; ind_y++) {
             neighbors = getNumLiveNeighbours(liveArray, [ind_x, ind_y]);
 
-            if(isLive(liveArray, [ind_x, ind_y])){
-                
-                if(neighbors==2 || neighbors==3){
+            if (isLive(liveArray, [ind_x, ind_y])) {
+                if (neighbors == 2 || neighbors == 3) {
                     newArr.push([ind_x, ind_y]);
                 }
-            } else{
-                if(neighbors==3){
+            } else {
+                if (neighbors == 3) {
                     newArr.push([ind_x, ind_y]);
                 }
             }
         }
     }
-
     return newArr;
+};
+
+function updateCanvas(canvasCtx, liveArray) {
+
+    drawRectangle(canvasCtx, liveArray)    
+    liveArray = updateLiveArray(liveArray);
+    drawPixels(canvasCtx, liveArray, "#a2a4a6");
+    
+    return liveArray;
+};
+
+function startGame() {
+    // liveCells = updateCanvas(context, liveCells);
+    // console.log(liveCells);
+    myTimer = setInterval(function () {
+        liveCells = updateCanvas(context, liveCells)
+    },
+        interval);
+
+}
+
+function stopGame() {
+    clearInterval(myTimer);
 }
 
 for (x = 0.5; x < canvasWidth; x += cellSize) {
@@ -128,11 +180,15 @@ for (y = 0.5; y < canvasHeight; y += cellSize) {
 }
 
 
-context.strokeStyle = "#515357";
+context.strokeStyle = "#97999c";
 context.stroke();
 
 canvas.addEventListener('mousedown', function (e) {
     getCursorPosition(canvas, e, liveCells, context);
 });
 
+startBtn = document.getElementById("Start");
+startBtn.addEventListener('click', startGame);
 
+stopBtn = document.getElementById("Stop");
+stopBtn.addEventListener('click', stopGame);
